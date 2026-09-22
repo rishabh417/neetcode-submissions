278f5fa3b97e6f7adb0a2975c923cdec6119f3c4
rishabh417 <84315979+rishabh417@users.noreply.github.com>
2026-09-22 19:09:37 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        // first i will create a copy of strs array and sort characters of the string in it
+        // then i will create a hashmap which will store (string,list of indices of anagrams of string)
+        // then i will create list and replace indices with real value from the original string 
+
+        HashMap<String,List<String>> map = new LinkedHashMap<>();
+
+        // List<List<String>> ans = new ArrayList<>();
+
+        for(String s : strs){
+
+            char [] charArs = s.toCharArray();
+            Arrays.sort(charArs);
+            String sortedKey = new String(charArs);
+
+            if(map.containsKey(sortedKey)){
+                map.get(sortedKey).add(s);
+            }
+            else{
+                List<String> ls = new ArrayList<>();
+                ls.add(s);
+                map.put(sortedKey,ls);
+            }
+
+        }
+
+        // for(Map.Entry<String,List<String>> entry : map.entrySet()){
+        //     ans.add(entry.getValue());
+        // }
+        
+        return new ArrayList<>(map.values());
+    }
+}
